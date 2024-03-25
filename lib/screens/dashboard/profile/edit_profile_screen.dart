4d13443b8a65a4984a70a4abc2d4ee98ac0c771c
nvsick <nvs.holy@gmail.com>
2024-03-25 14:16:06 +0300

    add editProfileScreen

--- lib/screens/dashboard/profile/edit_profile_screen.dart
+++ lib/screens/dashboard/profile/edit_profile_screen.dart
@@ -1,10 +1,129 @@
+import 'package:csevent/core/app_export.dart';
+import 'package:csevent/widgets/app_bar/appbar_leading_image.dart';
+import 'package:csevent/widgets/app_bar/appbar_subtitle.dart';
+import 'package:csevent/widgets/app_bar/custom_app_bar_image.dart';
+import 'package:csevent/widgets/custom_elevated_button.dart';
+import 'package:csevent/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class EditProfileScreen extends StatelessWidget {
-  const EditProfileScreen({super.key});
+  EditProfileScreen({super.key});
+
+  TextEditingController nameController = TextEditingController();
+
+  TextEditingController emailController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: _buildAppBar(context),
+        body: SizedBox(
+          width: SizeUtils.width,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 47.h,
+                  vertical: 7.v,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomImageView(
+                      imagePath: ImageConstant.imgNotFound,
+                      height: 188.adaptSize,
+                      width: 188.adaptSize,
+                      alignment: Alignment.center,
+                    ),
+                    SizedBox(
+                      height: 12.v,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Изменить фото",
+                        style: theme.textTheme.labelLarge,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.v,
+                    ),
+                    Text(
+                      "Имя пользователя",
+                      style: theme.textTheme.bodySmall,
+                    ),
+                    SizedBox(
+                      height: 6.v,
+                    ),
+                    CustomTextFormField(
+                      controller: nameController,
+                      hintText: "Иван Иванов",
+                    ),
+                    SizedBox(
+                      height: 16.v,
+                    ),
+                    Text(
+                      "Email",
+                      style: theme.textTheme.bodySmall,
+                    ),
+                    SizedBox(
+                      height: 6.v,
+                    ),
+                    CustomTextFormField(
+                      controller: emailController,
+                      hintText: "example@mail.com",
+                      textInputAction: TextInputAction.done,
+                      textInputType: TextInputType.emailAddress,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: _buidlSaveButton(context),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return CustomAppBar(
+      leadingWidth: 45.h,
+      leading: AppbarLeadingImage(
+        imagePath: ImageConstant.imgNotFound,
+        margin: EdgeInsets.only(
+          left: 18.h,
+          top: 16.v,
+          bottom: 19.v,
+        ),
+      ),
+      centerTitle: true,
+      title: AppbarSubtitle(
+        text: "Редактировать аккаунт",
+      ),
+    );
+  }
+
+  Widget _buidlSaveButton(BuildContext context) {
+    return CustomElevatedButton(
+      text: "Сохранить",
+      margin: EdgeInsets.only(
+        left: 47.h,
+        right: 47.h,
+        bottom: 47.v,
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
   }
 }
