a0c73e5d74c54038f5177138522c60f9bd42f774
nvsick <nvs.holy@gmail.com>
2024-03-29 01:59:31 +0300

    add dashboard icons

--- lib/core/utils/image_constant.dart
+++ lib/core/utils/image_constant.dart
@@ -6,4 +6,28 @@ class ImageConstant {
   static const String backButton = '$_imagePath/back_button.svg';
 
   static const String profileButton = '$_imagePath/profile_button.svg';
+
+  static const String offProductsScreenButton =
+      '$_imagePath/off_products_screen_button.png';
+
+  static const String offWarehousesScreenButton =
+      '$_imagePath/off_warehouses_screen_button.png';
+
+  static const String offEventsScreenButton =
+      '$_imagePath/off_events_screen_button.png';
+
+  static const String offMembersScreenButton =
+      '$_imagePath/off_members_screen_button.png';
+
+  static const String onProductsScreenButton =
+      '$_imagePath/on_products_screen_button.png';
+
+  static const String onWarehousesScreenButton =
+      '$_imagePath/on_warehouses_screen_button.png';
+
+  static const String onEventsScreenButton =
+      '$_imagePath/on_events_screen_button.png';
+
+  static const String onMembersScreenButton =
+      '$_imagePath/on_members_screen_button.png';
 }
